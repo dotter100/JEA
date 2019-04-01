4800f4db1eb4629ceddf3254f2d6f381a9725d00
Dotter <bart.van.dongen@hotmail.com>
2019-04-01 09:01:17 +0200

    Portfolio endpoint

---
+++ src/main/java/Stocks/Controlleres/Stocks/PortfolioController.java
@@ -0,0 +1,64 @@
+package Stocks.Controlleres.Stocks;
+
+
+
+
+import Stocks.JWT.Authenticated.AuthenticatedUser;
+import Stocks.JWT.JWT;
+import Stocks.Logic.Validator;
+import Stocks.Models.Portfolio;
+import Stocks.Models.Stocks;
+import Stocks.Models.User;
+import Stocks.Services.PortfolioService;
+import Stocks.Services.StockService;
+
+import javax.inject.Inject;
+import javax.servlet.annotation.ServletSecurity;
+import javax.ws.rs.*;
+import java.util.List;
+
+@Path("Portfolio")
+//@RolesAllowed("USER")
+@JWT
+public class PortfolioController {
+   @Inject
+   public Validator validator;
+
+    @Inject
+    private PortfolioService portfolioService;
+
+    @Inject
+    @AuthenticatedUser
+    User user;
+
+    @GET
+    @Produces("application/json")
+    public List<Portfolio> GetPortfolios(){
+        return user.getPortfolios();
+
+    }
+
+
+    @POST
+    @JWT
+    @Consumes("application/json")
+    @Produces("application/json")
+    public List<Portfolio> GetPortfolioName(String portfolioname){
+        return portfolioService.GetPortfolio(portfolioname);
+    }
+
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public List<Portfolio> GetPortfolioID(int id){
+        return portfolioService.GetPortfolio(id);
+    }
+
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public boolean UpdatePortfolio(Portfolio portfolio){
+        return portfolioService.UpdatePortfolio(portfolio);
+    }
+
+}
